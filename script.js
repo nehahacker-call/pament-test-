cd3857983d74237abce012fa9011b1c49efddfed
Rinku biswas <bikubiswas334@gmail.com>
2026-06-20 10:03:45 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,71 +1,41 @@
-let pan = document.getElementById("pan");
-let qty = document.getElementById("qty");
-
-
-function calculate(){
-
-let price = Number(pan.value);
-
-let quantity = Number(qty.value);
-
-
-let total = price * quantity;
-
-
-document.getElementById("amount").innerHTML =
-"Total Amount: ₹"+total;
-
-}
-
-
-pan.onchange = calculate;
-
-qty.oninput = calculate;
-
-
-
 function makePayment(){
 
+let name = document.getElementById("name").value;
+let mobile = document.getElementById("mobile").value;
+let date = document.getElementById("date").value;
 
-let name =
-document.getElementById("name").value;
+let price = Number(document.getElementById("pan").value);
+let qty = Number(document.getElementById("qty").value);
 
-
-let mobile =
-document.getElementById("mobile").value;
-
-
-let date =
-document.getElementById("date").value;
-
-
-let amount =
-Number(pan.value) * Number(qty.value);
-
+let amount = price * qty;
 
 
 if(name=="" || mobile=="" || date==""){
-
-alert("Please fill all details");
-
-return;
-
+    alert("Please fill all details");
+    return;
 }
 
 
+// তোমাৰ UPI ID
+let upiID = "zxfamily81@oksbi;
 
-let bookingID =
-Math.floor(Math.random()*100000);
+// তোমাৰ নাম
+let payeeName = "aszx";
 
 
+// UPI payment link
+let upiURL =
+"upi://pay?pa="+upiID+
+"&pn="+payeeName+
+"&am="+amount+
+"&cu=INR";
 
-document.getElementById("result").innerHTML=
 
-"✅ Booking Created <br><br>"+
-"Booking ID: "+bookingID+
-"<br>Name: "+name+
-"<br>Amount: ₹"+amount+
-"<br><br>Scan QR and Pay";
+// Mobile ত UPI app open
+window.location.href = upiURL;
 
+
+document.getElementById("result").innerHTML =
+"Booking Amount ₹"+amount;
 
 }
