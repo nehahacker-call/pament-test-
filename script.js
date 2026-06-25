224fa5a5c7959d7e7c99389629589401a410c5e4
Rinku biswas <bikubiswas334@gmail.com>
2026-06-25 08:06:39 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,55 +1,82 @@
 function makePayment(){
 
+
 let name = document.getElementById("name").value.toUpperCase();
+
 
 let mobile = document.getElementById("mobile").value;
 
+
+
 let panSelect = document.getElementById("pan");
+
 
 let panName = panSelect.options[panSelect.selectedIndex].text;
 
+
+
 let price = Number(panSelect.value);
 
+
+
 let qty = Number(document.getElementById("qty").value);
+
+
 
 let amount = price * qty;
 
 
-// Auto date
+
+
+// Auto Date
 
 let date = new Date().toLocaleDateString();
 
 
 
+
+// Name check
+
 if(name==""){
-    alert("Enter name");
-    return;
+
+alert("Enter Name");
+
+return;
+
 }
 
 
+
+
+// Mobile check
 
 if(!/^[0-9]{10}$/.test(mobile)){
 
-    alert("Mobile number must be 10 digit");
+alert("Mobile number must be 10 digit");
 
-    return;
+return;
 
 }
 
 
 
-let bookingID =
-Math.floor(Math.random()*100000);
+
+// Booking ID
+
+let bookingID = Math.floor(Math.random()*100000);
 
 
 
-// Google Sheet URL
+
+
+// =======================
+// GOOGLE SHEET SAVE
+// =======================
+
 
 let scriptURL = "https://script.google.com/macros/s/AKfycbzWNs-aJfh51_9hl3wa8Ckyw0-Ro1NHY6YH45Egik_Yp2_s_7uYEcnfPIJuxykH_9XbHQ/exec";
 
 
-
-// Save data to Google Sheet
 
 fetch(scriptURL,{
 
@@ -87,11 +114,18 @@ console.log("Saved:",data);
 
 
 
-// UPI Details
+
+
+// =======================
+// UPI PAYMENT
+// =======================
+
 
 let upiID = "rahulbidwas725@okicici";
 
+
 let shopName = "PAN SHOP";
+
 
 
 
@@ -106,22 +140,28 @@ let upiLink =
 
 
 
-// Device check
+
+// =======================
+// DEVICE CHECK
+// =======================
+
 
 let isMobile = /Android|iPhone|iPad|iPod/i.test(navigator.userAgent);
+
 
 
 
 if(isMobile){
 
 
-    // UPI app open try
-
-    window.location.assign(upiLink);
+    // Mobile UPI open
 
 
+    window.location.href = upiLink;
 
-    // 3 second পিছত Payment Screen show
+
+
+    // 3 second later show screen
 
     setTimeout(function(){
 
@@ -138,11 +178,11 @@ if(isMobile){
 
 
 }
-
 else{
 
 
-    // Laptop হলে direct Payment Screen
+    // Laptop Payment Screen
+
 
     showPaymentScreen(
         amount,
@@ -157,22 +197,40 @@ else{
 
 
 
-// Payment Screen Function
+}
+
+
+
+
+
+
+
+// =======================
+// PAYMENT SCREEN
+// =======================
+
 
 function showPaymentScreen(amount,upiID,bookingID){
+
 
 
 document.body.innerHTML = `
 
 
 <div style="
+
 text-align:center;
+
 margin-top:50px;
+
 font-family:Arial;
+
 ">
 
 
+
 <h2>💳 Payment</h2>
+
 
 
 <h3>
@@ -180,27 +238,39 @@ Pay Amount: ₹${amount}
 </h3>
 
 
+
+
 <img src="upi-qr.png" width="250">
 
 
+
+
+
 <p>
-UPI ID: ${upiID}
+UPI ID : ${upiID}
 </p>
 
 
+
 <p>
-Booking ID: ${bookingID}
+Booking ID : ${bookingID}
 </p>
 
 
+
+
 <p>
-Scan QR Code and Pay</p>
+Scan QR Code and Pay
+</p>
+
+
 
 
 </div>
 
 
 `;
+
 
 
 }
